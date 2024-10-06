4382b548ff257b31dab8696f1c6577e31f32cbea
Aslıhan ESEN <aslyqc14@gmail.com>
2024-10-07 00:30:59 +0300

    Proje dosyası ekle.

---
+++ Pratik - Lists 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Boş bir kahve listesi oluşturuyoruz
+        List<string> kahveler = new List<string>();
+
+        // Kullanıcıdan 5 kahve ismi girmesini istiyoruz
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.Write($"Kahve {i}: ");
+            string kahve = Console.ReadLine();
+            kahveler.Add(kahve);  // Kullanıcının girdiği kahveyi listeye ekliyoruz
+        }
+
+        // Kahveleri ekrana yazdırıyoruz
+        Console.WriteLine("\nKahve Listesi:");
+        foreach (string kahve in kahveler)
+        {
+            Console.WriteLine(kahve);
+        }
+    }
+}
+
